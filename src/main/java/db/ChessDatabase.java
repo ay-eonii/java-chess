a38ed2893a0091a790ee3e2e083871583ce98ed1
ay-eonii <naayen@naver.com>
2024-04-01 14:31:29 +0900

    feat(docker-compose): mysql 도커 사용

--- src/main/java/db/ChessDatabase.java
+++ src/main/java/db/ChessDatabase.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class ChessDatabase {
 
-    private static final String SERVER = "localhost:3306";
+    private static final String SERVER = "localhost:13306";
     private static final String DATABASE = "chess";
     private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "zncldkzl4369!";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
 
     public Connection getConnection() {
         try {
